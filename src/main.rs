ce7c69f4aed028cec3e29dfecd944edf2e126717
rolag <rolag@users.noreply.github.com>
2015-11-17 01:56:42 +0000

    Add basic argument parser that doesn't use get_opts

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,131 @@
-extern crate getopts; // https://doc.rust-lang.org/getopts/getopts/index.html
-use getopts::Options;
-use std::env;
+use std::{io, process, env};
+use std::io::prelude::*;
+use std::str;
+mod cnf_system;
+use cnf_system::{CNFSystem, CNFClause};
+mod DPLL;
 
 // Show help and exit
-fn show_help(program_name: &str, opts: Options) {
-    let brief = format!("Usage: {} [options] [FILE|enter system on prompt]", program_name);
-    print!("{}", opts.usage(&brief));
+fn show_help(program_name: String) {
+    println!("Usage: {} [options]", program_name);
+    println!("Mandatory arguments to long options are mandatory for short options too.");
+    println!("
+-c, --check-sat         Checks if the input system is a tautology, satisfiable
+                        or unsatisfiable.
+-i, --input-type TYPE   Input the system as using TYPE format. Possible values
+                        are 'dimacs'. Default: dimacs.
+-f, --file FILE         Read in the system from FILE. If FILE is ``-'', then
+                        input is read from stdin. Default: ``-''.
+-m, --models            Returns all models of the system or print unsatisfiable.
+-v, --version           Output version and exit, regardless of other arguments.
+-h, -?, --help          Output usage and exit, regardless of other arguments."
+            );
 }
 
 fn show_version() {
     println!("print license and version here");
 }
 
-fn set_options(opts: &mut Options) {
-    opts.optflag("m", "model",      "Print a model of the system, or 'unsatisfiable'.");
-    opts.optflag("M", "all-models", "Print all models of the system, or 'unsatisfiable'.");
-    opts.optflag("T", "tautology",  "Print if the system is a tautology and list all its models.");
-    opts.optopt("i",  "input-type", "Use the following format to input the system.", "dimacs");
-    opts.optopt("o",  "output",     "Output to the specified file, or stdout if FILE is ``-''.",
-                "FILE");
-    opts.optflag("h", "help",       "Print help menu and exit, regardless of other arguments.");
-    opts.optflag("v", "version",    "Print out version and exit, regardless of other arguments.");
+fn check_option(program_name: String, option: &String, option_values: Vec<&str>, option_name: &str) {
+    let mut option_is_valid = false;
+    for each_value in &option_values {
+        if each_value == option {
+            option_is_valid = true;
+            break;
+        }
+    }
+    if !option_is_valid {
+        error_and_exit(program_name,
+                       format!("invalid value for argument '{}': '{}'", option_name, option),
+                       22);
+    }
+}
+
+fn get_next_arg_or_err(program_name: String, args: &Vec<String>, current_index: usize) -> String {
+    let arg_count = args.len();
+    if current_index == arg_count - 1 {
+        error_and_exit(program_name,
+                       format!("necessary argument to {} not given", args[arg_count - 1]),
+                       22);
+    } else {
+        args[current_index + 1].clone()
+    }
+}
+
+fn error_and_exit(program_name: String, error_message: String, exit_code: i32) -> ! {
+    println!("{}: {}", program_name, error_message);
+    process::exit(exit_code);
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program_name = args[0].clone();
+    let arg_count: usize = args.len();
+    if arg_count == 1 {
+        show_help(program_name);
+        process::exit(0);
+    }
 
-    let mut opts = Options::new();
-    set_options(&mut opts);
-    let matches = match opts.parse(&args[1..]) {
-            Ok(all_matches) => { all_matches }
-            Err(failure)    => { panic!(failure.to_string()) }
-    };
-    if matches.opt_present("h") || args.len() == 1 {
-        show_help(&program_name, opts);
-        return;
-    };
-    if matches.opt_present("v") {
-        show_version();
-        return;
+    // Set argument defaults
+    let mut input_type = "dimacs".to_string();
+    let input_types = vec!["dimacs"];
+
+    let mut input_file = "-".to_string();
+
+    let mut check_type = "sat-check".to_string();
+    let check_types = vec!["sat-check", "model-check"];
+
+    // Loop through each argument, changing argument options when necessary
+    let mut arg_index: usize = 1;
+    while arg_index < arg_count {
+        match args[arg_index].as_ref() {
+            "-c" | "--check-sat" | "--sat-check" => {
+                check_type = "sat-check".to_string();
+            },
+            "-m" | "--models" | "--get-models" | "--get-all-models" => {
+                check_type = "model-check".to_string();
+            },
+            option if (option == "-i") | (option == "--input-type") => {
+                input_type = get_next_arg_or_err(program_name.clone(), &args, arg_index);
+                check_option(program_name.clone(), &input_type, input_types.clone(), option );
+                arg_index += 1;
+            },
+            "-f" | "--file" => {
+                input_file = get_next_arg_or_err(program_name.clone(), &args, arg_index);
+                arg_index += 1;
+            },
+            "-v" | "--version" => {
+                show_version();
+                process::exit(0);
+            },
+            "-h" | "-?" | "--help" => {
+                show_help(program_name);
+                process::exit(0);
+            },
+            bad_option => {
+                error_and_exit(program_name,
+                               format!("not and option: {}", bad_option),
+                               22
+                               );
+            },
+        }
+        arg_index += 1;
+    }
+
+    // TODO: implement algorithm for getting all models in CNF form
+    if check_type == "model-check" {
+        println!("model check not implemented yet");
+        process::exit(38);
+    }
+
+    // Find if the system is satisfiable or unsatisfiable or tautology
+    if input_file == "-" {
+        // Read in CNF system from stdin in dimacs form, for now
+        let input = io::stdin();
+        let system = CNFSystem::new(None);
+    } else {
+        // TODO: Read from file
+        println!("reading from file not implemented yet");
+        process::exit(38);
     }
 }
